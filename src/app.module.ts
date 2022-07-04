03c3064905565b3b3957273d3596f45547ff7301
cheichey <cheichey.1223@gmail.com>
2022-07-04 16:47:33 +0900

    chore: 環境変数を使用するようにした

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiModule } from './api/api.module';
-import { TypeormConfigService } from './typeorm-config/typeorm-config.service';
+import { TypeormConfigService } from './config/typeorm-config.service';
 import { MercariModule } from './mercari/mercari.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { PartModule } from './part/part.module';
 import { DiscordModule as baseDM } from '@discord-nestjs/core';
-import { Intents } from 'discord.js';
 import { DiscordModule } from './discord/discord.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { LoggerModule } from './logger/logger.module';
+import {ConfigModule} from "@nestjs/config";
+import {DiscordConfigService} from "./config/discord-config.service";
 
 @Module({
   imports: [
@@ -26,18 +27,10 @@ import { LoggerModule } from './logger/logger.module';
     PartModule,
     DiscordModule,
     EventEmitterModule.forRoot(),
+    ConfigModule.forRoot({isGlobal: true}),
     baseDM.forRootAsync({
-      useFactory: () => ({
-        token:
-          'OTkxOTA4NTYxOTA5MTk4OTE4.GzQF6m.s5JMuSKDidz2v1UPRGY9blJ3nlhuRC5fgEVrbU',
-        discordClientOptions: {
-          intents: [
-            Intents.FLAGS.GUILDS,
-            Intents.FLAGS.GUILD_MESSAGES,
-            Intents.FLAGS.GUILD_VOICE_STATES,
-          ],
-        },
-      }),
+      imports: [ConfigModule],
+      useClass: DiscordConfigService,
     }),
     LoggerModule,
   ],
---
+++ src/config/discord-config.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@nestjs/common";
+import {DiscordModuleOption, DiscordOptionsFactory} from "@discord-nestjs/core";
+import {ConfigService} from "@nestjs/config";
+
+@Injectable()
+export class DiscordConfigService implements DiscordOptionsFactory {
+  createDiscordOptions(): DiscordModuleOption {
+    const configService = new ConfigService();
+    return {
+      token: configService.get<string>('DISCORD_TOKEN'),
+      discordClientOptions: {
+        intents: ["GUILD_MESSAGES"]
+      },
+    }
+  }
+}

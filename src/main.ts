8aa850b4508109c39e2a9a72c592d4f2e8f3c1d4
cheichey <cheichey.1223@gmail.com>
2022-07-21 13:18:49 +0900

    feat: CORS を許可した

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { LoggerService } from './logger/logger.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(LoggerService));
+  app.enableCors({
+    origin: "*",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept"
+  })
   setUpApiDocs(app);
   await app.listen(3000);
 }

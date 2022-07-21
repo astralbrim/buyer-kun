72544b147becadec1d628c2f0546c3bc8c171203
cheichey <cheichey.1223@gmail.com>
2022-07-21 23:41:21 +0900

    change: partService.add() の引数の型を配列でなくした

--- src/part/part.service.ts
+++ src/part/part.service.ts
@@ -18,15 +18,49 @@ export class PartService {
   }
 
   async add(
-    partsData: {name: string,
+    name: string,
     minPrice: number,
     maxPrice: number,
-    type: ProductType}[]
-  ): Promise<PartEntity[]> {
-    const parts = partsData.map(({name,minPrice, maxPrice, type}) => PartEntity.create(name, minPrice, maxPrice, type))
-    const result = await this.partRepository.save(parts);
+    type: ProductType
+  ): Promise<PartEntity> {
+    const part = PartEntity.create(name, minPrice, maxPrice, type);
+    const result = await this.partRepository.save(part);
     this.eventEmitter.emit('part.add');
     return result;
+  }
+
+  async addAndUpdate(parts: {name: string,
+    type: ProductType,
+    minPrice: number,
+    maxPrice: number;}[]) {
+    const currentParts = await this.getAll();
+    const newParts = parts.filter((part) => {
+      return currentParts.every((current) => current.name != part.name)
+    })
+    const deletedPart = currentParts.filter(
+      (current) => {
+        let isDeleted = true;
+        parts.forEach(
+          (part) => {
+            if(part.name == current.name) isDeleted = false
+          }
+        )
+        return isDeleted;
+      }
+    )
+    for (const {name, minPrice, maxPrice, type} of newParts) {
+      await this.add(name, minPrice, maxPrice, type);
+    }
+    for (const {name} of deletedPart) {
+      await this.delete(name);
+    }
+    parts.forEach(
+      ({name, minPrice, maxPrice, type}) => {
+        this.updateSetting(name, minPrice, maxPrice, type);
+      }
+    )
+    return await this.getAll();
+
   }
 
   async getAll(): Promise<PartEntity[]> {
@@ -46,12 +80,14 @@ export class PartService {
     name: string,
     minPrice: number,
     maxPrice: number,
+    type: ProductType
   ): Promise<PartEntity | null> {
     const part = await this.getByName(name);
     if (!part) return null;
     part.name = name;
     part.minPrice = minPrice;
     part.maxPrice = maxPrice;
+    part.type = type;
     return await this.partRepository.save(part);
   }
 

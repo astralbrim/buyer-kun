7a97ec13d45a221e339028fbb35ce9c30189bf46
cheichey <cheichey.1223@gmail.com>
2022-08-14 17:02:56 +0900

    chore: ポートを環境変数から取得するようにした

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
   setUpApiDocs(app);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 void bootstrap();
